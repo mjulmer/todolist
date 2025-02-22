56ddcbcfa0b532bb2d0885bb96be691acca075a6
M Ulmer <mju9999@gmail.com>
2025-02-21 20:27:18 -0800

    Add 'schema' for storing lists in local storage + intro functions.
    This commit includes the function declarations for everything I
    think I'll need, but only DEFINES the easy functions. It also
    moves the object parsing logic out of the todo-classes file.
    
    I decided against storing the todos separately and referencing
    them because it would be a lot of trouble to manually stringify
    the list metadata EXCLUDING the todos (only having their IDs), and
    the only list metadata separate from the todos is the name -- as
    the "next todo ID" field needs to be updated as well.
    
    If this were a production app, it would be worth it: I'd have a
    more robust way to generate unique IDs for todos and lists, and
    the list name and list of TODOs would be stored separately...
    but in a production app I wouldn't be writing to this single
    bucket local storage table.

--- src/storage-manager.js
+++ src/storage-manager.js
@@ -1,8 +1,27 @@
 "use strict";
 
-export { isStoragePopulated, getColorTheme, setColorTheme };
+export {
+  isStoragePopulated,
+  getColorTheme,
+  setColorTheme,
+  getListIds,
+  updateListofListIds,
+};
+
+/**
+ * Contains functions for managing writing to and from local storage.
+ *
+ * Storage key schema:
+ *
+ * colorTheme: The preferred color theme, "light" or "dark".
+ * listIds: A list of the IDs of all of the non-dailies lists or "projects".
+ * list-$listId: List metadata (e.g. name, next todo ID, list of todos) for the
+ *    list with the specified ID.
+ */
 
 const colorThemeKey = "colorTheme";
+const listIdsKey = "listIds";
+const listKeyPrefix = "list-";
 
 function isStoragePopulated() {
   return localStorage.getItem(colorThemeKey) !== null;
@@ -27,4 +46,44 @@ function setColorTheme(theme) {
     return;
   }
   localStorage.setItem(colorThemeKey, theme);
+}
+
+/** Returns a list of all list IDs. Does not include the dailies list. */
+function getListIds() {
+  return JSON.parse(localStorage.getItem(listIdsKey));
+}
+
+// returns a list ITEM with todos fully "hydrated"
+// add a note to docstring about todos
+function getList(listID) {}
+
+/** Takes an object containing all lists (not including the dailies list)
+ * and updates the list of list IDs in local storage.
+ */
+function updateListofListIds(lists) {
+  localStorage.setItem(listIdsKey, Object.keys(lists));
+}
+
+// on any list or todo change
+function updateList(list) {}
+
+function parseTodoListFromJson(jsonObject) {
+  const list = new TodoList(jsonObject.name, jsonObject.description);
+  list.id = jsonObject.id;
+  list.itemIdCount = jsonObject.itemIdCount;
+
+  for (const todoIndex in jsonObject.todos) {
+    list.todos.push(parseTodoItemFromJson(jsonObject.todos[todoIndex]));
+  }
+  return list;
+}
+
+function parseTodoItemFromJson(jsonObject) {
+  const todo = new TodoItem(
+    jsonObject.id,
+    jsonObject.name,
+    jsonObject.description
+  );
+  todo.completed = jsonObject.completed;
+  return todo;
 }
